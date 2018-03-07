a4fa75eceeb5cfefd377373b13aaed1334210023
Lukas Forst <forstluk@fel.cvut.cz>
2018-03-07 16:01:15 +0100

    lab 3

--- labs/3/integrate.cpp
+++ labs/3/integrate.cpp
@@ -3,15 +3,18 @@
 #include <omp.h>
 #include <cmath>
 
+using namespace std;
+
 double
 integrate_sequential(std::function<double(double)> integrand, double a, double step_size, int step_count) {
-
     // Promenna kumulujici obsahy jednotlivych obdelniku
     double acc = 0.0;
 
-    //TODO doplnte kod
-    throw "Not implemented yet";
-    
+    for (auto i = 1; i < step_count; i++) {
+        auto cx = a + (2 * i + 1) * step_size / 2;
+        acc += integrand(cx) * step_size;
+    }
+
     // Celkovy obsah aproximuje hodnotu integralu funkce
     return acc;
 }
@@ -20,13 +23,13 @@ double
 integrate_omp_critical(std::function<double(double)> integrand, double a, double step_size, int step_count) {
     double acc = 0.0;
 
-    //TODO
     // Rozdelte celkovy interval na podintervaly prislusici jednotlivym vlaknum
     // Identifikujte kritickou sekci, kde musi dojit k zajisteni konzistence mezi vlakny
     throw "Not implemented yet";
 
     return acc;
 }
+
 
 double
 integrate_omp_atomic(std::function<double(double)> integrand, double a, double step_size, int step_count) {
@@ -35,7 +38,14 @@ integrate_omp_atomic(std::function<double(double)> integrand, double a, double s
     //TODO
     // Rozdelte celkovy interval na podintervaly prislusici jednotlivym vlaknum
     // Identifikujte kritickou sekci, kde musi dojit k zajisteni konzistence mezi vlakny
-    throw "Not implemented yet";
+
+
+#pragma omp parallel for
+    for (auto i = 1; i < step_count; i++) {
+        auto cx = a + (2 * i + 1) * step_size / 2;
+#pragma omp atomic
+        acc += integrand(cx) * step_size;
+    }
 
     return acc;
 }
@@ -46,7 +56,11 @@ double integrate_omp_reduction(std::function<double(double)> integrand, double a
     //TODO
     // Rozdelte celkovy interval na podintervaly prislusici jednotlivym vlaknum
     // Identifikujte kritickou sekci, kde musi dojit k zajisteni konzistence mezi vlakny
-    throw "Not implemented yet";
+#pragma omp parallel for reduction(+:acc)
+    for (auto i = 1; i < step_count; i++) {
+        auto cx = a + (2 * i + 1) * step_size / 2;
+        acc += integrand(cx) * step_size;
+    }
 
     return acc;
 }
@@ -57,8 +71,12 @@ double integrate_omp_for_static(std::function<double(double)> integrand, double 
 
     //TODO
     //rozsirte integrate_omp_reduction o staticke rozvrhovani
-    throw "Not implemented yet";
-    
+#pragma omp parallel for reduction(+:acc) schedule(static)
+    for (auto i = 1; i < step_count; i++) {
+        auto cx = a + (2 * i + 1) * step_size / 2;
+        acc += integrand(cx) * step_size;
+    }
+
     // Celkovy obsah aproximuje hodnotu integralu funkce
     return acc;
 }
@@ -69,8 +87,12 @@ double integrate_omp_for_dynamic(std::function<double(double)> integrand, double
 
     //TODO
     //rozsirte integrate_omp_reduction o dynamicke rozvrhovani
-    throw "Not implemented yet";
-    
+#pragma omp parallel for reduction(+:acc) schedule (dynamic)
+    for (auto i = 1; i < step_count; i++) {
+        auto cx = a + (2 * i + 1) * step_size / 2;
+        acc += integrand(cx) * step_size;
+    }
+
     // Celkovy obsah aproximuje hodnotu integralu funkce
     return acc;
 }
