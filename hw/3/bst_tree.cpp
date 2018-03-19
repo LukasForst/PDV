c513efd42e39aaf94561bcea05d9137d4d72f67f
Lukas Forst <forstluk@fel.cvut.cz>
2018-03-19 21:39:42 +0100

    solution for hw3

--- hw/3/bst_tree.cpp
+++ hw/3/bst_tree.cpp
@@ -2,14 +2,42 @@
 #include "bst_tree.h"
 
 void bst_tree::insert(long long data) {
+    auto new_node = new node(data);
+
+    auto *curr = root;
+    if (curr == nullptr) {
+        root = new_node;
+        return;
+    }
+
+    while (curr != nullptr && curr->data != data) {
+        node *possible_next = nullptr;
+        if (curr->data < data) {
+            possible_next = curr->right;
+            if (possible_next == nullptr && curr->right.compare_exchange_strong(possible_next, new_node)) {
+                return;
+            }
+        } else {
+            possible_next = curr->left;
+            if (possible_next == nullptr && curr->left.compare_exchange_strong(possible_next, new_node)) {
+                return;
+            }
+        }
+        curr = possible_next;
+    }
+
+    throw "UnexpectedError";
 }
 
 bst_tree::~bst_tree() {
-    std::function<void(node*)> cleanup = [&](node * n) {
-        if(n != nullptr) {
+    // Rekurzivni funkce pro pruchod stromu a dealokaci pameti prirazene jednotlivym
+    // uzlum
+    std::function<void(node *)> cleanup = [&](node *n) {
+        if (n != nullptr) {
             cleanup(n->left);
-            delete n;
             cleanup(n->right);
+
+            delete n;
         }
     };
     cleanup(root);
