9b931e9e6cb279338d6d99ef68ff85012321cd04
Lukas Forst <lukas.forst@blindspot.ai>
2018-04-09 16:07:23 +0200

    add first term paper

---
+++ termpapers/sem_01/algorithms/bfs.cpp
@@ -0,0 +1,12 @@
+#include "bfs.h"
+
+// Naimplementujte efektivni algoritmus pro nalezeni nejkratsi cesty v grafu.
+// V teto metode nemusite prilis optimalizovat pametove naroky, a vhodnym algo-
+// ritmem tak muze byt napriklad pouziti prohledavani do sirky (breadth-first
+// search.
+//
+// Metoda ma za ukol vratit ukazatel na cilovy stav, ktery je dosazitelny pomoci
+// nejkratsi cesty.
+std::shared_ptr<const state> bfs(std::shared_ptr<const state> root) {
+    return root;
+}
